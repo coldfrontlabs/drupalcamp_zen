1bc74e66d1eaa1184d9cacc2ac1a637cdec97316
Tiffany Tse <ttse@coldfrontlabs.ca>
2013-09-11 16:54:09 -0400

    Added files and fixed theme compatibility issues

---
+++ templates/region--footer.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the footer region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728140
+ */
+?>
+
+<?php if ($content): ?>
+  <footer id="footer" class="<?php print $classes; ?>">
+    <div class="wrapper">
+      <?php print $content; ?>
+      <ul>
+        <li class="fb"><a href="https://www.facebook.com/DrupalCampOttawa" target="_blank">Facebook</a></li>
+        <li class="twitter"><a href="https://twitter.com/DrupalCampOTT" target="_blank">Twitter</a></li>
+      </ul>
+      <p class="colophon">Copyright &copy; 2013 <a href="http://drupalcampottawa.ca">Drupal Camp Ottawa</a></p>
+    </div> <!-- footer wrapper ends -->
+  </footer> <!-- footer ends-->
+<?php endif; ?>
